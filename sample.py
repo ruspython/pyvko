bf49099ad47ed51c548862833ef03eb918fbf5e2
Vladislav <vlad0058@gmail.com>
2014-08-20 22:37:44 +0400

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ import json
 
 APP_ID = '3313789'
 SCOPE = '8388607' #all (sum([2**x for x in range(23)])) 
-mytoken, user_id = vk.auth(email='vlad0058@gmail.com', password='%8h\'T+Wj1', client_id=APP_ID, scope=SCOPE)
+mytoken, user_id = vk.auth(email='email', password='password', client_id=APP_ID, scope=SCOPE)
 print('\n', mytoken, user_id)
 d = {'user_id': user_id, 'count':'14'}
 json_obj = json.loads(vk.method_request(token=mytoken, method_name='audio.get', params=d).decode())
